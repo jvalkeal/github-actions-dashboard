59756564c8254ce3cde50bd0a5aa032f201297cd
Janne Valkealahti <janne.valkealahti@gmail.com>
2020-01-26 13:51:16 +0000

    Initial

---
+++ src/main/java/io/spring/githubactionsdashboard/GithubActionsDashboardApplication.java
@@ -0,0 +1,13 @@
+package io.spring.githubactionsdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spring/githubactionsdashboard/GithubActionsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.githubactionsdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
