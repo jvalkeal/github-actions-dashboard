51f2cb53d8ec9e919b01dc6f938b6684515c952a
Janne Valkealahti <janne.valkealahti@gmail.com>
2020-06-27 16:21:11 +0100

    Polish

--- github-actions-dashboard-server/src/main/java/io/spring/githubactionsdashboard/controller/RepositoryDispatchController.java
+++ github-actions-dashboard-server/src/main/java/io/spring/githubactionsdashboard/controller/RepositoryDispatchController.java
@@ -16,7 +16,6 @@
 package io.spring.githubactionsdashboard.controller;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
